3414c1a43785b7bc54434a71603cc4dff3ab072e
Bulgaru Valeriu <valeri.bulgaru06@gmail.com>
2026-03-23 17:37:10 +0200

    am adaugat fisierul "app.component.ts"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { ToastComponent } from './shared/components/toast/toast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
-  template: `<router-outlet></router-outlet>`
+  imports: [RouterOutlet, ToastComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'easy-apply-frontend';
+}
