2fe0d07fc92b0a2b7ea439c701ee1e03b6b6e82b
Bulgaru Valeriu <valeri.bulgaru06@gmail.com>
2026-05-10 01:19:11 +0300

    feat: create analytics data service

---
+++ src/app/services/analytics.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { CompanyStatistics } from '../../models/company.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnalyticsService {
+  private apiUrl = `${environment.apiUrl}/company`;
+
+  constructor(private http: HttpClient) { }
+
+  getCompanyAnalytics(companyId: string, timeframe: string = 'weekly'): Observable<CompanyStatistics> {
+    return this.http.get<CompanyStatistics>(`${this.apiUrl}/${companyId}/analytics`, {
+      params: { timeframe }
+    });
+  }
+}
